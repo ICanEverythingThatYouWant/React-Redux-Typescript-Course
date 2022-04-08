de4c234c5f33270eda3d7996e13af948cfd62fef
Admin <glazko17@yandex.ru>
2022-04-08 03:00:15 +0300

    add hooks and todoReducer

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -2,12 +2,15 @@ import React, {FC, useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {useTypedSelector} from "../hooks/useTypedSelector";
 import {fetchUsers} from "../store/action-creators/user";
+import {useActions} from "../hooks/useActions";
 
 const UserList: FC = () => {
     const {users, error, loading} = useTypedSelector(state => state.user)
-    const dispatch = useDispatch()
+    const {fetchUsers} = useActions()
+
+
     useEffect( () =>{
-        dispatch(fetchUsers())
+        fetchUsers()
     }, [])
 
     if (loading) {
